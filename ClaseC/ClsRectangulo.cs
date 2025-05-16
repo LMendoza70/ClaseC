788e883454d14d35e5a324a8fdca7b197bfff5b5
@LMendoza70 <luis.mendoza@uthh.edu.mx>
2025-05-16 11:19:44 -0600

    first commit

---
+++ ClaseC/ClsRectangulo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseC
+{
+    class ClsRectangulo
+    {
+        //atrubutos
+        private float Alto;
+        private float Ancho;
+
+        //definimos propiedades
+        public float getAlto()
+        {
+            return Alto;
+        }
+        public void setAlto(float value)
+        {
+            Alto = value;
+        } 
+        public float getAncho()
+        {
+            return Ancho;
+        }
+
+        public void setAncho(float value)
+        {
+            Ancho = value;
+        }
+
+        //metodo constructor
+        public ClsRectangulo()
+        {
+            Ancho = 0;
+            Alto = 0;
+        }
+        //definimos metodos
+        public float Area()
+        {
+            float res = 0;
+            res = Alto * Ancho;
+            return res;
+        }
+
+        public float Perimetro()
+        {
+            float res = 0;
+            res = (Alto + Ancho) * 2;
+            //res = Alto + Alto + Ancho + Ancho;
+            return res;
+        }
+    }
+}
---
+++ ClaseC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creamos el objeto de la clase rectangulo
+            ClsRectangulo miRectangulo = new ClsRectangulo();
+
+            //capturamos los valores de alto y ancho
+            Console.WriteLine("Captura el ancho que tendra el rectangulo");
+            miRectangulo.setAncho(float.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Captura el Alto del rectangulo");
+            miRectangulo.setAlto(float.Parse(Console.ReadLine()));
+
+            Console.WriteLine("El area del rectangulo es : " +miRectangulo.Area()+" m2");
+            Console.WriteLine("El Perimetro del rectangulo es : "+miRectangulo.Perimetro()+" m");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
